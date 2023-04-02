36d2a00cb2867000c2fc23511431581c116cf643
Srishty M <srishtymangutte@gmail.com>
2023-04-02 23:58:00 +0000

    initial commit

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/Bodycontain.css';
 import { LeftSection } from './LeftSection';
 import { MidSection } from './MidSection';
+import { RightSection } from './RightSection';
 
 export const BodyContainer = () => {
   return (
     <div className='bodycontain'>
         <LeftSection/>
         <MidSection/>
+        <RightSection/>
     </div>
   )
 }
---
+++ src/components/RightGrp.js
@@ -0,0 +1,29 @@
+import './styles/RightSection.css';
+import EastIcon from '@mui/icons-material/East';
+
+export const RightGrp=({data})=>{
+    return(
+        <div className='rightgrp'>
+            <h4 className='grpheading'>{data.grphead} <EastIcon/>
+            </h4>
+            {
+                data.grpcont.map(sp=>(
+                    <SmallPost content={sp}/>
+                ))
+            }
+        </div>
+    )
+}
+
+const SmallPost=({content})=>{
+    return(
+        <p className="smallpost">
+           {content.imgsrc && <img className="spimg" src={content.imgsrc}></img>}
+        
+            <p className='sppara'>
+            <h5 className="spheading">{content.sphead}</h5>
+            {content.textpara}
+            </p>
+        </p>
+    )
+}
